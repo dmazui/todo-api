2cf9dcad96b41374ff146dba5827452f3eb0a7a3
diogo <dev.diogo@gmail.com>
2024-04-23 15:40:18 -0300

    Db off

--- src/main/java/com/dmazui/todoapi/model/Tarefa.java
+++ src/main/java/com/dmazui/todoapi/model/Tarefa.java
@@ -2,44 +2,44 @@ package com.dmazui.todoapi.model;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.Lob;
-import javax.persistence.Table;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Index;
+//import javax.persistence.Lob;
+//import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
+//@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "tarefas", indexes = {@Index(name = "tarefa_index", columnList = "id", unique = true)})
+//@Table(name = "tarefas", indexes = {@Index(name = "tarefa_index", columnList = "id", unique = true)})
 public class Tarefa {
 
-	@Id
-	@Column(nullable = false)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+//	@Id
+//	@Column(nullable = false)
+//	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	
-	@Column(length = 60)
+//	@Column(length = 60)
 	private String titulo;
 
-	@Lob
+//	@Lob
 	private String descricao;
 	
-	@Column
+//	@Column
 	private LocalDateTime dataDeCriacao;
 	
-	@Column
+//	@Column
 	private LocalDateTime dataDeComclusao;
 	
-	@Column
+//	@Column
 	private boolean concluido;
 
 }
--- src/main/java/com/dmazui/todoapi/repositories/TarefaRepository.java
+++ src/main/java/com/dmazui/todoapi/repositories/TarefaRepository.java
@@ -1,11 +1,11 @@
 package com.dmazui.todoapi.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dmazui.todoapi.model.Tarefa;
 
 @Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+public interface TarefaRepository /*extends JpaRepository<Tarefa, Long> */{
 
 }
